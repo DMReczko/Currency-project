6186b74d6dd02923b6d5c75326b6199a30c6d65b
DMReczko <117109879+DMReczko@users.noreply.github.com>
2023-02-22 19:16:14 +0100

    fixes

--- js/index.js
+++ js/index.js
@@ -2,34 +2,25 @@ const currency = document.querySelector("#currencies");
 const btn = document.querySelector("#convert-btn");
 const result = document.querySelector("#result");
 const loader = document.querySelector("#loading");
-
-function displayLoading() {
-    loader.classList.add("display");
-}
-
-function hideLoading() {
-    loader.classList.remove("display");
-}
+const userValue = document.querySelector("#multiply-value");
 
 function convert() {
     const url = `https://api.nbp.pl/api/exchangerates/rates/a/${currency.value}/`;
-
-    const value = parseFloat(document.querySelector("#multiply-value").value);
+    const value = parseFloat(userValue.value);
 
     if (isNaN(value)) {
         return alert("Wprowadź poprawną kwotę");
     } else {
-        displayLoading();
+        loader.classList.add("display");
         fetch(url)
             .then((response) => response.json())
 
             .then((data) => {
-                hideLoading();
+                loader.classList.remove("display");
                 const multiplier = parseFloat(data.rates[0].mid);
                 const converted = parseFloat((value * multiplier).toFixed(2));
                 result.innerText = converted;
             })
-
             .catch((err) => console.error(err));
     }
 }
